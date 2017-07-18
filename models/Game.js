ef9c7a2303b6fc2cbc43a76bc0882342530f43a7
Bohdan Levantovych <bohdan.levantovych@eleks.com>
2017-07-18 18:10:54 +0300

    add mongoose model for game

---
+++ models/Game.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// TL - Top Left, TC - Top Center, TR - Top Right,
+// CL - Center Left, CC - Center Center, CR - Center Right,
+// BL - Bottom Left, BC - Bottom Center, BR - Bottom Right,
+
+const gameSchema = mongoose.Schema({
+    playerX: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+    playerO: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+    winner: { type: mongoose.Schema.Types.ObjectId, ref: 'Player' },
+    createdAt: { type: Date },
+    moves: [ String ] // ex. ['CC', 'CR', ...]
+});
+
+module.exports = mongoose.model('Game', gameSchema);
