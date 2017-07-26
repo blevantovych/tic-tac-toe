a0e355479e43e4e4b11a2db48889803b404c7c45
Bohdan Levantovych <bohdan.levantovych@eleks.com>
2017-07-26 15:06:42 +0300

    add winner checking

--- public/bundle.js
+++ public/bundle.js
@@ -21,6 +21,7 @@ exports.getColumn = (twoDimArr, colNum) => twoDimArr.map((el, i) => twoDimArr[i]
 },{}],3:[function(require,module,exports){
 require('./bling');
 const { getSecondaryDiagonal, getMainDiagonal, getRow, getColumn } = require('./helpers');
+const getters = [getSecondaryDiagonal, getMainDiagonal, getRow, getColumn];
 
 const board = [
     ['', '', ''],
@@ -30,12 +31,34 @@ const board = [
 
 let moves = 0;
 
+const allValuesTheSame = arr => arr.indexOf('') === -1 && new Set(arr.filter(el => el !== '')).size === 1;
+
+function checkDiagonals(board) {
+    return allValuesTheSame(getMainDiagonal(board)) || allValuesTheSame(getSecondaryDiagonal(board));
+}
+
+function checkRows(board) {
+    return board.map(allValuesTheSame).some(el => el === true)
+}
+
+function checkColumns(board) {
+    return board.map((el, i) => allValuesTheSame(getColumn(board, i))).some(el => el === true);
+}
+
+function checkWin(board) {
+    if ([checkColumns, checkRows, checkDiagonals].map(f => f(board)).some(el => el === true)) {
+        console.log('There is a winner!');
+    } else {
+        console.log('No winner yet');
+    }
+}
+
 $('td').on('click', function() {
     const [x, y] = this.getAttribute('data-pos').split('');
-    board[x][y] = ++moves % 2 ? 'X' : '0';
-    console.table(board);
+    const char = ++moves % 2 ? 'X' : '0';
+    board[x][y] = char;
+    checkWin(board);
+    this.textContent = char;
 });
-
-
 
 },{"./bling":1,"./helpers":2}]},{},[3]);
--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 require('./bling');
 const { getSecondaryDiagonal, getMainDiagonal, getRow, getColumn } = require('./helpers');
+const getters = [getSecondaryDiagonal, getMainDiagonal, getRow, getColumn];
 
 const board = [
     ['', '', ''],
@@ -9,10 +10,32 @@ const board = [
 
 let moves = 0;
 
+const allValuesTheSame = arr => arr.indexOf('') === -1 && new Set(arr.filter(el => el !== '')).size === 1;
+
+function checkDiagonals(board) {
+    return allValuesTheSame(getMainDiagonal(board)) || allValuesTheSame(getSecondaryDiagonal(board));
+}
+
+function checkRows(board) {
+    return board.map(allValuesTheSame).some(el => el === true)
+}
+
+function checkColumns(board) {
+    return board.map((el, i) => allValuesTheSame(getColumn(board, i))).some(el => el === true);
+}
+
+function checkWin(board) {
+    if ([checkColumns, checkRows, checkDiagonals].map(f => f(board)).some(el => el === true)) {
+        console.log('There is a winner!');
+    } else {
+        console.log('No winner yet');
+    }
+}
+
 $('td').on('click', function() {
     const [x, y] = this.getAttribute('data-pos').split('');
-    board[x][y] = ++moves % 2 ? 'X' : '0';
-    console.table(board);
+    const char = ++moves % 2 ? 'X' : '0';
+    board[x][y] = char;
+    checkWin(board);
+    this.textContent = char;
 });
-
-
