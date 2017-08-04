dc22d2144943364343fb38da4defe41687f1e38d
Bohdan Levantovych <bohdan.levantovych@eleks.com>
2017-08-04 10:56:18 +0300

    remove junk from server.js

--- server.js
+++ server.js
@@ -6,5 +6,3 @@ const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
-    // "build": "browserify public/main.js -o public/bundle.js",
-    // "watch": "watchify public/main.js -o public/bundle.js -v"
